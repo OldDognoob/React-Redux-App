612cdeccaa3baf6a3e693b0bbbd68babae741fcd
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-19 16:56:28 +0200

    create-step-3-4-5-6

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
+import {combineReducers, createStore, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {pokemonReducer, spinnerReducer} from './state/reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//STEP-4 USE combineReducers FROM REDUX TO MAKE A SINGLE REDUCER
+const combinedReducer = combineReducers({
+    pokemon: pokemonReducer,
+    spinnerOn: spinnerReducer,
+})
+
+//STEP-5 USE createStore FROM REDUX TO MAKE A STATE STORE
+const store = createStore(
+    combinedReducer,
+    {},
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+
+ReactDOM.render(
+    //STEP-6 WRAP THE APPLICATION WITH A PROVIDER FROM REACT-REDUX
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- myapp/src/state/reducers.js
+++ myapp/src/state/reducers.js
@@ -5,4 +5,38 @@ import * as types from './actionTypes'
 const initialState = {
     pokemon:[],
     isFetching: false,
+}, 
+spinnerON: false,
+
+//STEP-3 CREATE ONE REDUCER FUNCTION PER SLICE
+const initialStateForm = {pokemon: [], isFetching:false}
+export function pokemonReducer(state=initialStateForm, action){
+    switch(action.type){
+        case types.GET_POKEMON_START:
+            return{
+                ...state,
+                pokemon:action.payload,
+                isFetching: true
+            }
+        case types.GET_POKEMON_SUCCESS:
+            return{
+                ...state,
+                pokemon:action.payload,
+                isFetching: false
+            }
+        default:
+            return state;
+    }
+}
+
+const initialStateSpinner = false;
+export function spinnerReducer(state=initialStateSpinner, actions){
+    switch(action.type){
+        case types.SPINNER_START:
+            return true;
+        case types.SPINNER_STOP:
+            return initialStateSpinner;
+            default:
+                return state;
+    }
 }
